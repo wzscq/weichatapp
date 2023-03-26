192e445ee66c87b893db614040d37541862864cb
wangzhongshu <wzscq@126.com>
2023-03-26 13:36:58 +0800

    commit changes

--- service/public/TextMessageHandler.go
+++ service/public/TextMessageHandler.go
@@ -4,6 +4,10 @@ import (
 	"weichatapp/common"
 )
 
+const (
+	MSG_GETID="我的ID"
+)
+
 type TextMessageHandler struct {
 	OpenaiproxyConf common.OpenaiproxyConf
 	ChatCompletionHandler ChatCompletionHandler
@@ -14,7 +18,12 @@ type ChatCompletionHandler interface {
 }
 
 func (h *TextMessageHandler)HandleMessage(msg *MessageRequest){
-	answer:=h.ChatCompletionHandler.ChatCompletion(msg.FromUserName,msg.Content)
+	var answer string
+	if msg.Content==MSG_GETID {
+		answer=msg.FromUserName
+	} else {
+		answer=h.ChatCompletionHandler.ChatCompletion(msg.FromUserName,msg.Content)
+	}
 	//发送微信客户消息
 	PostCSMessage(msg.FromUserName,answer)
 }
